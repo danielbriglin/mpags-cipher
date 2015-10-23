6f82431659f758ae67eb01e7104b2d09e4f3c911
Daniel Briglin <daniel.lawrence.briglin@cern.ch>
2015-10-23 14:29:09 +0100

    Ex.2

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,1 +1,31 @@
-d
+#include <iostream>
+
+
+
+int main()
+{
+  //this is a comment!!! 
+  /* This is a multiline comment*/
+  std::cout<< "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n";
+
+  int a{4}, b{8}, c{0};
+  a=++a;
+  c = a*b;
+
+  std:: cout << c << "\n"; 
+
+  const double s{5.5012};
+
+  std:: cout <<s <<"\n"; 
+
+  std:: cout<< s*a << "\n";
+  
+  auto p{true};
+
+  //std:: cout<<p<< "\n";
+
+  std:: cout<< s/a <<"\n";
+
+  std:: cout<< a/b<<"\n";
+
+}
